00a7c534fa7f70e2d497f5b508b534bf0553d8c4
nayanrdeveloper <nayanrdeveloper@gmail.com>
2025-02-23 15:21:43 +0530

    chore: setup Prettier for code formatting

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-white text-3xl rounded-lg p-2 bg-green-400 font-extrabold border-l-emerald-800">Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Text className="text-white text-3xl rounded-lg p-2 bg-green-400 font-extrabold border-l-emerald-800">
+                Edit app/index.tsx to edit this screen.
+            </Text>
+        </View>
+    );
 }
--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    presets: [require('nativewind/preset')],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
